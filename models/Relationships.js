9e495e50957bee399b92116f56990e8e0314a2a9
Luqman Hassan <luqman1isb@gmail.com>
2025-06-27 11:37:22 +0500

    add some import to relationship file

--- models/Relationships.js
+++ models/Relationships.js
@@ -12,64 +12,40 @@ const Fuel = require("./Fuel");
 const Maintenance = require("./Maintenance");
 const userRole = require("./userRole");
 const permission = require("./permission");
+const AssignPermissionToRoles = require("./assignPermissionToRole");
+const AssignRolesToUser = require("./assignRolesToUser");
+const user = require("./User");
 
 function setupRelationships() {
   // Ministry has many Departments
-  Ministry.hasMany(Department, {
-    foreignKey: "ministryId",
-    onDelete: "CASCADE",
-  });
+  Ministry.hasMany(Department, {foreignKey: "ministryId",onDelete: "CASCADE",});
   Department.belongsTo(Ministry, { foreignKey: "ministryId" });
   // Department has many Employees
-  Department.hasMany(Employee, {
-    foreignKey: "departmentId",
-    onDelete: "CASCADE",
-  });
+  Department.hasMany(Employee, {foreignKey: "departmentId",onDelete: "CASCADE",});
   Employee.belongsTo(Department, { foreignKey: "departmentId" });
   //car department
   Department.hasMany(Car, { foreignKey: "departmentId", onDelete: "CASCADE" });
   Car.belongsTo(Department, { foreignKey: "departmentId" });
   // **Driver belongs to Department**
-  Department.hasMany(Driver, {
-    foreignKey: "departmentId",
-    onDelete: "CASCADE",
-  });
+  Department.hasMany(Driver, {foreignKey: "departmentId",onDelete: "CASCADE",});
   Driver.belongsTo(Department, { foreignKey: "departmentId" });
   // Car-Driver Reservation Relationship
-  Car.hasMany(CarDriverReservation, {
-    foreignKey: "carId",
-    onDelete: "CASCADE",
-  });
-  Driver.hasMany(CarDriverReservation, {
-    foreignKey: "driverId",
-    onDelete: "CASCADE",
-  });
+  Car.hasMany(CarDriverReservation, {foreignKey: "carId",onDelete: "CASCADE",});
+  Driver.hasMany(CarDriverReservation, {foreignKey: "driverId",onDelete: "CASCADE",});
   CarDriverReservation.belongsTo(Car, { foreignKey: "carId" });
   CarDriverReservation.belongsTo(Driver, { foreignKey: "driverId" });
   // Car-Employee History Relationship (Fixed foreign key)
   Car.hasMany(carEmployeeHistory, { foreignKey: "carId", onDelete: "CASCADE" });
-  Employee.hasMany(carEmployeeHistory, {
-    foreignKey: "employeeId",
-    onDelete: "CASCADE",
-  });
+  Employee.hasMany(carEmployeeHistory, {foreignKey: "employeeId",onDelete: "CASCADE",});
   carEmployeeHistory.belongsTo(Car, { foreignKey: "carId" });
   carEmployeeHistory.belongsTo(Employee, { foreignKey: "employeeId" });
   // Employee-Car Allocation Relationship (Fixed foreign key and capitalization)
-  Employee.hasMany(employeeCarAllocation, {
-    foreignKey: "employeeId",
-    onDelete: "CASCADE",
-  });
-  Car.hasMany(employeeCarAllocation, {
-    foreignKey: "carId",
-    onDelete: "CASCADE",
-  });
+  Employee.hasMany(employeeCarAllocation, {foreignKey: "employeeId",onDelete: "CASCADE",});
+  Car.hasMany(employeeCarAllocation, {foreignKey: "carId",onDelete: "CASCADE",});
   employeeCarAllocation.belongsTo(Employee, { foreignKey: "employeeId" });
   employeeCarAllocation.belongsTo(Car, { foreignKey: "carId" });
   // Driver-Car History Relationship
-  Driver.hasMany(DriverCarHistory, {
-    foreignKey: "driverId",
-    onDelete: "CASCADE",
-  });
+  Driver.hasMany(DriverCarHistory, {foreignKey: "driverId",onDelete: "CASCADE",});
   Car.hasMany(DriverCarHistory, { foreignKey: "carId", onDelete: "CASCADE" });
   DriverCarHistory.belongsTo(Driver, { foreignKey: "driverId" });
   DriverCarHistory.belongsTo(Car, { foreignKey: "carId" });
@@ -82,30 +58,12 @@ function setupRelationships() {
   //maintenance-car
   Car.hasMany(Maintenance, { foreignKey: "carId", onDelete: "CASCADE" });
   Maintenance.belongsTo(Car, { foreignKey: "carId" });
-
   // userRole and permission relationship
-  userRole.belongsToMany(permission, {
-    through: AssignPermissionToRoles,
-    foreignKey: "userRoleId",
-    otherKey: "permissionId",
-  });
-  permission.belongsToMany(userRole, {
-    through: AssignPermissionToRoles,
-    foreignKey: "permissionId",
-    otherKey: "userRoleId",
-  });
-
+  userRole.belongsToMany(permission, {through: AssignPermissionToRoles,foreignKey: "userRoleId",otherKey: "permissionId",});
+  permission.belongsToMany(userRole, {through: AssignPermissionToRoles,foreignKey: "permissionId",otherKey: "userRoleId",});
   // user and userRole relationship
-  user.belongsToMany(userRole, {
-    through: AssignRolesToUser,
-    foreignKey: "userId",
-    otherKey: "userRoleId",
-  });
-  userRole.belongsToMany(user, {
-    through: AssignRolesToUser,
-    foreignKey: "userRoleId",
-    otherKey: "userId",
-  });
+  user.belongsToMany(userRole, {through: AssignRolesToUser,foreignKey: "userId",otherKey: "userRoleId",});
+  userRole.belongsToMany(user, {through: AssignRolesToUser,foreignKey: "userRoleId",otherKey: "userId",});
 }
 
 module.exports = setupRelationships;
